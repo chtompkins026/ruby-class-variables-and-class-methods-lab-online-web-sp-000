11e3988dbde769d4d6e1032b0d9d93804afd4bf1
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-23 16:12:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,17 @@
 class Song
   @@count = 0 
+  @@genres = [] 
   
   def intialize(name, artist, genre)  
     @name, @artist, @genre = name, artist, genre 
     @@count += 1 
+    @@genres << genre unless @@genres.include?(genre)
   end 
   
   def count 
     @@count 
   end 
+  
+  
   
 end #end of the song class   
