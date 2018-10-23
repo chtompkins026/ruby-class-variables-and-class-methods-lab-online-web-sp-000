f44b707535ac93ce20f281fcfe79a2b5d64afa6f
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-23 16:11:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,8 @@
 class Song
   @@count = 0 
-  def intialize(name, artist, genre)  
   
+  def intialize(name, artist, genre)  
+    @name, @artist, @genre = name, artist, genre 
+  end 
   
 end #end of the song class   
